ce16dd5c7e68cfc8155f83af13430819e068d514
HagarMagdy <hagarmagdyr@gmail.com>
2018-04-06 14:09:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hagar/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.hagar.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.txt_view) TextView textView;
+    @BindView(R.id.button) Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+
+
+    }
+
+    @OnClick(R.id.button)
+    void buttonClick() {
+        textView.setText("Button Clicked");
+
+    }
+
+
+
+}
